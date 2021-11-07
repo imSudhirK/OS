7b9c1f0b162d07a3080567fa8e18693037b24372
Sudhir Kumar <42682048+imSudhirK@users.noreply.github.com>
2021-11-07 15:05:17 +0530

    Add files via upload

---
+++ OS-Lab03/tc_mmap.c
@@ -0,0 +1,70 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+// int a[10000];
+
+int main(void)
+{
+  uint ret;
+  printf(1, "Start: memory usage in pages: virtual: %d, physical %d\n", numvp(), numpp());
+
+
+  // printf(1, "Start: memory usage in pages: virtual: %d, physical %d\n", numvp(), numpp());
+
+  // for(int i=0; i<10000; i++){
+  //   a[i] = i;
+  // }
+  
+  // printf(1, "%p\n", a);
+
+  // printf(1, "Start: memory usage in pages: virtual: %d, physical %d\n", numvp(), numpp());
+
+  ret = mmap (-1234);
+  if(ret == 0)
+    printf(1, "mmap failed for wrong inputs\n");
+  else
+    exit();
+  
+  ret = mmap (1234);
+  if(ret == 0)
+    printf(1, "mmap failed for wrong inputs\n");
+  else
+    exit();
+
+  
+  ret = mmap(4096);
+  
+  if(ret == 0 )
+    printf(1, "mmap failed\n");
+  else {
+    printf(1, "After mmap one page: memory usage in pages: virtual: %d, physical %d\n", numvp(), numpp());
+
+    char *addr = (char *) ret;
+
+    addr[0] = 'a';
+    
+    printf(1, "After access of one page: memory usage in pages: virtual: %d, physical %d\n", numvp(), numpp());
+  }
+
+  ret = mmap(8192);
+
+  if(ret == 0 )
+    printf(1, "mmap failed\n");
+  else {
+    printf(1, "After mmap two pages: memory usage in pages: virtual: %d, physical %d\n", numvp(), numpp());
+
+    char *addr = (char *) ret;
+
+    addr[0] = 'a';
+    
+    printf(1, "After access of first page: memory usage in pages: virtual: %d, physical %d\n", numvp(), numpp());
+    addr[8000] = 'a';
+
+    printf(1, "After access of second page: memory usage in pages: virtual: %d, physical %d\n", numvp(), numpp());
+  }
+
+  exit();
+ 
+}
